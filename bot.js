c19127d0699262cd7eb20c8d01bc2c47bbdcbdd6
kwangric <43795498+kwangric@users.noreply.github.com>
2022-10-17 14:00:54 -0400

    First commit

---
+++ bot.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('messageCreate', async (msg) => {
+  if (msg.author.bot) return
+  if (msg.content === 'woof') {
+    msg.reply('woof')
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
